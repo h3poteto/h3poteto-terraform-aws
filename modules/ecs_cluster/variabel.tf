6f39ba2fcd3085ef11f475611bf92ae6d54e0d83
AkiraFukushima <h3.poteto@gmail.com>
2018-12-16 19:18:41 +0900

    Update AMIs

--- modules/ecs_cluster/variabel.tf
+++ modules/ecs_cluster/variabel.tf
@@ -66,7 +66,7 @@ variable "ecs_task_cleanup_wait_duration" {
  AutoscalingGroup
 */
 variable "instance_type" {
-  default = "t2.micro"
+  default = "t3.medium"
 }
 
 variable "ec2_instance_profile_arn" {}
