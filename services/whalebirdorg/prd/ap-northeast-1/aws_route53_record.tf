8a19389c4450fa17ead2f1e0396366b9549bd47d
AkiraFukushima <h3.poteto@gmail.com>
2019-02-05 00:47:51 +0900

    Remove whalebirdorg route53

--- services/whalebirdorg/prd/ap-northeast-1/aws_route53_record.tf
+++ services/whalebirdorg/prd/ap-northeast-1/aws_route53_record.tf
@@ -1,11 +1,1 @@
-resource "aws_route53_record" "whalebird_org" {
-  zone_id = "${var.whalebird_org_host_zone_id}"
-  name    = "whalebird.org"
-  type    = "A"
 
-  alias {
-    name                   = "${data.terraform_remote_state.base_prd_tokyo.lb_dns_name}"
-    zone_id                = "${data.terraform_remote_state.base_prd_tokyo.lb_zone_id}"
-    evaluate_target_health = true
-  }
-}
