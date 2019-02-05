e4edcdc34c071a31cc8b821a0b640ad57c318681
AkiraFukushima <h3.poteto@gmail.com>
2019-02-05 23:59:37 +0900

    Move pleroma to EKS

--- services/pleroma/prd/ap-northeast-1/aws_route53_record.tf
+++ services/pleroma/prd/ap-northeast-1/aws_route53_record.tf
@@ -1,15 +1,3 @@
-resource "aws_route53_record" "pleroma_io" {
-  zone_id = "${var.pleroma_io_host_zone_id}"
-  name    = "pleroma.io"
-  type    = "A"
-
-  alias {
-    name                   = "${data.terraform_remote_state.base_prd_tokyo.lb_dns_name}"
-    zone_id                = "${data.terraform_remote_state.base_prd_tokyo.lb_zone_id}"
-    evaluate_target_health = true
-  }
-}
-
 resource "aws_route53_record" "media" {
   zone_id = "${var.pleroma_io_host_zone_id}"
   name    = "media.pleroma.io"
