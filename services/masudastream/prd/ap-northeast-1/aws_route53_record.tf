32fa56cb4157f3cfdaf72a125c2ab81ad587da67
AkiraFukushima <h3.poteto@gmail.com>
2019-02-08 23:44:22 +0900

    Redirect masuda-stream.net to masuda.stream

--- services/masudastream/prd/ap-northeast-1/aws_route53_record.tf
+++ services/masudastream/prd/ap-northeast-1/aws_route53_record.tf
@@ -4,8 +4,8 @@ resource "aws_route53_record" "masuda_stream_net" {
   type    = "A"
 
   alias {
-    name                   = "${data.terraform_remote_state.base_prd_tokyo.lb_dns_name}"
-    zone_id                = "${data.terraform_remote_state.base_prd_tokyo.lb_zone_id}"
+    name                   = "${aws_cloudfront_distribution.masuda_stream_net.domain_name}"
+    zone_id                = "${aws_cloudfront_distribution.masuda_stream_net.hosted_zone_id}"
     evaluate_target_health = true
   }
 }
