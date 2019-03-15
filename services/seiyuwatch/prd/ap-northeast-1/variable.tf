46a395e666690198eb22c3aa81a1b1cd1a9dae14
AkiraFukushima <h3.poteto@gmail.com>
2019-03-15 21:54:19 +0900

    Add seiyuwatch route53

---
+++ services/seiyuwatch/prd/ap-northeast-1/variable.tf
@@ -0,0 +1,11 @@
+variable "seiyu_watch_host_zone_id" {
+  default = "ZPE08ZJUUIWUT"
+}
+
+variable "alb_dns_name" {
+  default = "9d7ac9a8-webpublic-albingr-4676-921123158.ap-northeast-1.elb.amazonaws.com"
+}
+
+variable "alb_hosted_zone_id" {
+  default = "Z14GRHDCWA56QT"
+}
