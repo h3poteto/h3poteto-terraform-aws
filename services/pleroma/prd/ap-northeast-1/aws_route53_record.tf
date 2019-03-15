a177f8d88fb4963723448e9c775355016f14bdd0
AkiraFukushima <h3.poteto@gmail.com>
2019-03-15 21:57:46 +0900

    Add pleromaio route53

--- services/pleroma/prd/ap-northeast-1/aws_route53_record.tf
+++ services/pleroma/prd/ap-northeast-1/aws_route53_record.tf
@@ -9,3 +9,15 @@ resource "aws_route53_record" "media" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_route53_record" "web" {
+  zone_id = "${var.pleroma_io_host_zone_id}"
+  name    = ""
+  type    = "A"
+
+  alias {
+    name                   = "${var.alb_dns_name}"
+    zone_id                = "${var.alb_hosted_zone_id}"
+    evaluate_target_health = true
+  }
+}
