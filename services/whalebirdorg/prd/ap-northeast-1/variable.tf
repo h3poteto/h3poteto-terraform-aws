accf8ea0de6c3ee16b6e6dbea1d8959ed3e3cd12
AkiraFukushima <h3.poteto@gmail.com>
2019-03-15 22:01:13 +0900

    Add whalebirdorg route53

---
+++ services/whalebirdorg/prd/ap-northeast-1/variable.tf
@@ -0,0 +1,11 @@
+variable "whalebird_org_host_zone_id" {
+  default = "Z13GST5UDMNVY7"
+}
+
+variable "alb_dns_name" {
+  default = "9d7ac9a8-webpublic-albingr-4676-921123158.ap-northeast-1.elb.amazonaws.com"
+}
+
+variable "alb_hosted_zone_id" {
+  default = "Z14GRHDCWA56QT"
+}
