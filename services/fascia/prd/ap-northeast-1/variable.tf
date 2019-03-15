da8fa0685d58b6dc0f5bac7f4e58007d0f43aa7a
AkiraFukushima <h3.poteto@gmail.com>
2019-03-15 21:40:42 +0900

    Add fascia route53

---
+++ services/fascia/prd/ap-northeast-1/variable.tf
@@ -0,0 +1,11 @@
+variable "fascia_io_zone_id" {
+  default = "Z317685ECO8XFK"
+}
+
+variable "alb_dns_name" {
+  default = "9d7ac9a8-webpublic-albingr-4676-921123158.ap-northeast-1.elb.amazonaws.com"
+}
+
+variable "alb_hosted_zone_id" {
+  default = "Z14GRHDCWA56QT"
+}
