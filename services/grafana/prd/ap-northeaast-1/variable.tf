25cce388958f1fbad20fa34a7371e1589445ab0d
AkiraFukushima <h3.poteto@gmail.com>
2019-05-05 11:54:08 +0900

    Add grafana service

---
+++ services/grafana/prd/ap-northeaast-1/variable.tf
@@ -0,0 +1,11 @@
+variable "h3poteto_dev_host_zone_id" {
+  default = "Z1C50OXUI4YHFA"
+}
+
+variable "alb_dns_name" {
+  default = "5736192a-webpublic-albingr-4676-1205031836.ap-northeast-1.elb.amazonaws.com"
+}
+
+variable "alb_hosted_zone_id" {
+  default = "Z14GRHDCWA56QT"
+}
