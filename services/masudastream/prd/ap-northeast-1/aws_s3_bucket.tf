19510c36d2e70c5de98091bae03fd67c2c4ba7dc
AkiraFukushima <h3.poteto@gmail.com>
2019-12-24 22:53:02 +0900

    Remove masuta-stream.net redirect configuration

--- services/masudastream/prd/ap-northeast-1/aws_s3_bucket.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "redirect_masuda_stream" {
-  bucket = "redirect.masuda-stream.net"
-  acl    = "public-read"
-
-  website {
-    redirect_all_requests_to = "https://masuda.stream"
-  }
-}
-
