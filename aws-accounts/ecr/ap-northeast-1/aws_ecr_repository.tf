c6be3178cddd99d22ff9fb1fd464a8ae0ebaff9a
AkiraFukushima <h3.poteto@gmail.com>
2020-07-23 22:36:52 +0900

    Remove unused ECR

--- aws-accounts/ecr/ap-northeast-1/aws_ecr_repository.tf
+++ aws-accounts/ecr/ap-northeast-1/aws_ecr_repository.tf
@@ -2,24 +2,12 @@ resource "aws_ecr_repository" "fascia" {
   name = "h3poteto/fascia"
 }
 
-resource "aws_ecr_repository" "ayanerer" {
-  name = "h3poteto/ayanerer"
-}
-
 resource "aws_ecr_repository" "seiyuwatch" {
   name = "h3poteto/seiyuwatch"
 }
 
-resource "aws_ecr_repository" "whalebirdorg" {
-  name = "h3poteto/whalebirdorg"
-}
-
 resource "aws_ecr_repository" "whalebirdsocial" {
   name = "h3poteto/whalebirdsocial"
-}
-
-resource "aws_ecr_repository" "hanazawabot" {
-  name = "h3poteto/hanazawabot"
 }
 
 resource "aws_ecr_repository" "masudastream" {
