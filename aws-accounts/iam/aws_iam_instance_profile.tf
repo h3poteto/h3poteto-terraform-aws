053d338ee17a77581fc7b33a2a3e59a0c6ce04d9
AkiraFukushima <h3.poteto@gmail.com>
2021-05-17 21:34:43 +0900

    Add playground k8s iam roles

--- aws-accounts/iam/aws_iam_instance_profile.tf
+++ aws-accounts/iam/aws_iam_instance_profile.tf
@@ -3,8 +3,18 @@ resource "aws_iam_instance_profile" "k8s_master_profile" {
   role = aws_iam_role.k8s_master_role.name
 }
 
+resource "aws_iam_instance_profile" "playground_k8s_master_profile" {
+  name = "playground_k8s-master-profile"
+  role = aws_iam_role.playground_k8s_master_role.name
+}
+
 resource "aws_iam_instance_profile" "k8s_node_profile" {
   name = "k8s-node-profile"
   role = aws_iam_role.k8s_node_role.name
 }
 
+
+resource "aws_iam_instance_profile" "playground_k8s_worker_profile" {
+  name = "playground-k8s-worker-profile"
+  role = aws_iam_role.playground_k8s_worker_role.name
+}
